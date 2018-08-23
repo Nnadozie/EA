bb0547dd9af66d6c49b66982d4c30e665ec7a2eb
Nnadozie Okeke <n.e.okeke@outlook.com>
2018-08-23 15:39:16 +0100

    evolution working

---
+++ ErrorLog.md
@@ -0,0 +1,5 @@
+## TypeError: Cannot read property 'length' of undefined
+Method expected a 4 element array but kept receiving a 3 element array
+Check method inputs and outputs carefully
+See that all methods are returning the correct value
+In this case a method was trying to return array[4] instead of array[3]
--- heyEvolve.js
+++ heyEvolve.js
@@ -10,97 +10,135 @@
 
 //Creating a gene pool
 var pool = [
-    "(&\"^£(&())BB&(D&(&&(V)))",
-    "LCCOIUV(**YHS IYS(&S(VPV",
-    ";;;;kjjj4444789/*6",
-    "kjvvv8u99a[]'#'/'[[#''[[#####'///';[[]][[[p[]]'#//'",
-    "jaf0ajfaafaft8aa5((£*££*^£(£*£(£*£",
-    "kjj v8bba ad0f9a a0dfaub0",
-    "Nnadozie Ekenechukwu Okeke"
+    " ik0002495n 02945 024525 0",
+    "02985289 0-295 2- 90252905",
+    "9ag890952390595959m9824099",
+    "6\\\"89898**%DZGGSh#77^**(%(",
+    "23452354q2544452452567^£&3",
+    "£%a&*$%GDH&^*$*$JFH^&DGH&&",
+    "%^£DFGHU^%&FJTY^&$GT $^$^h",
 ]
+
 
 //create fitness function
 //length is same as my full name
 //sum of first two, then 
 function checkFitness(gene) {
-    var count = 0
+    var fittest = "Nnadozie Ekenechukwu Okeke"
     var sum = 0
-    var num = 0
-    for(let i of gene){
-        num = i.charCodeAt(0)
-        if(count%2 == 0){
-            num = -num
-        }
-        sum += num
-        count++
+
+    for(let i = 0; i < gene.length; i++) {
+
+            if(fittest[i] != gene[i]) {
+                sum+=1
+            }
     }
-    return (sum/26)
+    return sum
 }
+
+////console.log(checkFitness("Nnadozie Ekenechukwu Okeke"))
+
 
 //check for fittest
 function chooseFittest(poo) {
+    ////console.log("got here 2")
     var rank = []
     var rankOb = {}
-    var calc
+    var calc = 0
     for(let i of poo) {
-        calc = checkFitness(i) - 3.5384615384615383
+        calc = checkFitness(i)
         if(calc < 0){calc = calc * -1}
         rank.push(calc)
+        //console.log(rank)
         rankOb[calc] = i
-        if(calc == 0){
-            stop = 1
-        }
+        //console.log(rankOb)
+        /*if(calc == 0){
+            ////console.log("got here 3")
+            stop = 1            
+            rank.sort((a, b)=>(a > b))
+            ////console.log(rankOb)
+            return([rankOb[rank[0]], rankOb[rank[1]], rankOb[rank[2]], rankOb[rank[3]]])
+
+        }*/
     }
     rank.sort((a, b)=>(a > b))
     //console.log(rankOb)
-    return([rankOb[rank[0]], rankOb[rank[1]], rankOb[rank[2]], rankOb[rank[4]]])
+   //console.log(` fittest: ${[rankOb[rank[0]], rankOb[rank[1]], rankOb[rank[2]], rankOb[rank[3]]]}`)
+    return([rankOb[rank[0]], rankOb[rank[1]], rankOb[rank[2]], rankOb[rank[3]]])
 }
 
-//console.log(chooseFittest(pool))
+////console.log(chooseFittest(pool))
 
 //cross over
-function randSlice(gene) {
-    var bin = Math.random() * (2 - 0)
-    var cut = Math.random() * (gene.length+1 - 0)
-    if(bin == 0) {
-        return gene.slice(0,cut)
-    }else{
-        return gene.slice(cut, gene.length+1)
-    }
+function randSlice(gene1, gene2) {
+    //var bin = Math.random() * (2 - 0)
+    var cut = Math.random() * (gene1.length - 1)
+    var off1 = gene1.slice(0,cut)
+    var off2 = gene2.slice(cut, gene2.length)
+    return (off1+off2)
+    
 }
 
 //for each fittest
 function breed(fittest) {
-    var offSpring = []
+    //var offSpring = []
     var mate = []
     
         for(let k = 0; k < 10; k++){
-            mate = []
-            for(let i of fittest) {
-                mate.push(randSlice(i))
-            }
-            //console.log(mate)
-            offSpring.push(mate[0]+mate[1])
-            offSpring.push(mate[0]+mate[2])
-            offSpring.push(mate[1]+mate[2])
+
+            mate.push(randSlice(fittest[0],fittest[1]))
+            mate.push(randSlice(fittest[0],fittest[2]))
+            mate.push(randSlice(fittest[0],fittest[3]))
+            mate.push(randSlice(fittest[1],fittest[2]))
+            mate.push(randSlice(fittest[1],fittest[3]))
+            mate.push(randSlice(fittest[2],fittest[3]))
+            mate.push(randSlice(fittest[1],fittest[0]))
+            mate.push(randSlice(fittest[2],fittest[0]))
+            mate.push(randSlice(fittest[3],fittest[0]))
+            mate.push(randSlice(fittest[2],fittest[1]))
+            mate.push(randSlice(fittest[3],fittest[1]))
+            mate.push(randSlice(fittest[3],fittest[2]))
     }
 
-    return offSpring
+    return mate
 }
 
 function mutate(offSpring) {
+    //console.log(offSpring)
     var mut, mut1 = 0
-    for(let i = 0; i < offSpring.length; i++){
-        mut = Math.random() * (offSpring[i].length+1 - 0)
-        mut1 = Math.random() * (offSpring[i].length+1 - 0)
 
-        offSpring[i] = (offSpring[i].toString()).replace((offSpring[i].toString()).charAt(mut), String.fromCharCode(mut + 1))
-        offSpring[i] = (offSpring[i].toString()).replace((offSpring[i].toString()).charAt(mut1), String.fromCharCode(mut1 - 1))
+    for(let i = 0; i < offSpring.length; i++){
+        sool = 'abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        sool = sool.split('')
+        mut = Math.random() * (offSpring[i].length - 1)
+        out = Math.random() * (sool.length - 1)
+        mut = Math.round(mut)
+        out = Math.round(out)
+        ////console.log(`os len: ${offSpring[i].length}`)
+        ////console.log(mut) 
+        mut1 = Math.random() * (offSpring[i].length - 1)
+        out2 = Math.random() * (sool.length - 1)
+        mut1 = Math.round(mut1)
+        out2 = Math.round(out2)
+        ////console.log(`os len 2: ${offSpring[i].length}`)
+        ////console.log(mut1)
+        ////console.log(offSpring[i][mut])
+        offSpring[i] = offSpring[i].split('')
+        //console.log(offSpring[i])
+        //console.log(`${mut} ${mut1}`)
+        //console.log(offSpring[i][mut])
+        offSpring[i][mut] = sool[out]
+        //offSpring[i][mut1] = sool[out2]        
+        //console.log(offSpring[i].toString())
+        offSpring[i] = offSpring[i].join('')
+        //console.log(offSpring[i])
+
+        //String.fromCharCode(s.charCodeAt() + 1)
         
     }
+    //console.log(offSpring)    
     return offSpring
 }
-
 
 //---randomly slice all fittest
 //choose between 0 and 1
@@ -117,22 +155,57 @@ function writeTf(test) {
         if (err) throw err;
         
         // success case, the file was saved
-        //console.log('Lyric saved!');
+        ////console.log('Lyric saved!');
     });
 }
 
+var stop = 0
+var j = 0
+while(stop == 0) {
+    //console.log(j)
+    //console.log(`start${pool}`)
+    //console.log(pool)    
+    var fittest = chooseFittest(pool)
+    var copy = fittest.map((n)=>{return n})
 
-for(let j = 0; j < 3000; j++) {
-    pool = chooseFittest(pool)
-    for(let i of pool) {
-        writeTf(j+": " + i)
+    for(let i of fittest) {
+        //console.log(j+": " + i)
+       writeTf(j+": " + i)
     }
-    pool = breed(pool)
-    for(let i of pool) {
-        writeTf(j+": " + i)
+
+    if (fittest[0] == "Nnadozie Ekenechukwu Okeke") {
+        //writeTf(j+": " + fittest[0])
+        console.log("FIT!")        
+        stop = 1
     }
+
+    //console.log(pool)
+    childs = breed(fittest)
+    ////console.log(pool)
+    childs = mutate(fittest)
+
+    pool = copy.concat(childs)
+    //console.log(pool)
+    j++
+    if(j==100000){break;}
 }
+console.log("got here 4")
 
+/*for(let j = 0; j < 3000; j++) {
+    pool = chooseFittest(pool)
+    pool = breed(pool)
+    pool = mutate(pool)
+    for(let i of pool) {
+        writeTf(j+": " + i)
+    }
+}*/
+
+
+/*pool = chooseFittest(pool)
+pool = breed(pool)
+//console.log(pool)
+pool = mutate(pool)
+//console.log(pool)
 /*
 var stop = 0
 var gen = 1
@@ -149,9 +222,9 @@ while(stop == 0){
 
 //writeTf(pool.toString())
 //pool = mutate(pool)
-//console.log(pool)
+////console.log(pool)
 gen++
 }
-console.log(stop)
+//console.log(stop)
 
-*/
+**/
